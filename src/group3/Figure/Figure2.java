c5535b2283d26fb24412fa07f6ca79eea58b9bba
Goharik Hovhannsiyan <gog.hovhannisyan.18@bk.ru>
2022-05-12 19:58:58 +0300

    Figure Home work2

--- src/group3/Figure/Figure2.java
+++ src/group3/Figure/Figure2.java
@@ -2,23 +2,23 @@ package group3.Figure;
 
 public class Figure2 {
     /*
-           *
-         * *
-       * * *
-     * * * *
-   * * * * *
+                *
+              * *
+            * * *
+          * * * *
+        * * * * *
      */
+
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            for (int j = 5; j > i; j--) {
+            for (int j = i; j <= 4; j++) {
                 System.out.print("  ");
+
             }
-            for (int j = 0; j <= i; j++) {
+            for (int j = 0; j <=i ; j++) {
                 System.out.print(" *");
             }
-            System.out.println(" ");
+            System.out.println("");
         }
-
     }
-
 }
---
+++ src/group3/FigureHomework/FirstFigure.java
@@ -0,0 +1,25 @@
+package group3.FigureHomework;
+
+public class FirstFigure {
+    /*
+          * * * *
+            * * *
+              * *
+                *
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+
+
+            }
+            for (int j = i; j <=3 ; j++) {
+                System.out.print(" *");
+            }
+
+            System.out.println(" ");
+        }
+    }
+}
+
